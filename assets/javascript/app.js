1728b16fdb09d18d14adfa21faa446290f0224c3
Elisabeth Edrington <edringtondc@gmail.com>
2018-12-05 17:32:22 -0700

    normal cards, no parallax

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,3 +4,12 @@
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems, options);
   });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.parallax');
+    var instances = M.Parallax.init(elems, options);
+  });
+
+  // Or with jQuery
+
+  var instance = M.Parallax.getInstance(elem);
